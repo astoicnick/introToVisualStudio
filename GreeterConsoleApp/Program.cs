2fcbe0aab561fbc54d4ad8370eb96af910a28b56
Nicholas A Perry <nicholasperrypro@gmail.com>
2020-08-19 17:19:42 -0400

    Add project files.

---
+++ GreeterConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeterConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usersName = GetName();
+            Console.WriteLine("Hello, " + usersName);
+            Console.ReadLine();
+        }
+        static string GetName()
+        {
+            Console.WriteLine("Hey! What's yo name?");
+            string name = Console.ReadLine();
+            return name;
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int myAge = 19;
+            myAge += 1;
+            Console.WriteLine("Hello I'm " + myAge);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int myDogsAge = 4;
+            myDogsAge += 1;
+            Console.WriteLine("My dog is " + myDogsAge);
+        }
+    }
+}
